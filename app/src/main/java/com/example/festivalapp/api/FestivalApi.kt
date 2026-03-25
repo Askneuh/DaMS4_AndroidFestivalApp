0069810a49717f5edf5ab0fe928c81f4a7836376
Daanoun <ydaanoun@gmail.com>
2026-03-25 09:06:07 +0100

    missing files

---
+++ app/src/main/java/com/example/festivalapp/api/FestivalApi.kt
@@ -0,0 +1,17 @@
+package com.example.festivalapp.api
+
+import retrofit2.http.POST
+import retrofit2.http.Body
+import retrofit2.http.GET
+
+interface FestivalApi {
+    @POST("login")
+    suspend fun login(@Body credentials: LoginRequest): LoginResponse
+    
+    @GET("items")
+    suspend fun getItems(): List<Item>
+}
+
+data class LoginRequest(val email: String, val password: String)
+data class LoginResponse(val token: String, val user: User)
+data class User(val id: Int, val name: String)
